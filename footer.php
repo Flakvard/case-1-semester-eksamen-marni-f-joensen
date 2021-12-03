844335e4e98b130968d35022973de6cc7ac016f7
Marni <marni@falkvard.com>
2021-12-03 21:03:31 +0100

    Hjemmesiden

---
+++ footer.php
@@ -0,0 +1,119 @@
+<div class="footer-wrapper">
+            <div class="cards-container infokort-cards-container">
+                <div class="card">
+                    <div class="card-info">
+                        <p class="card-overskrift">Unikke stole</p>
+                        <p>Vores stole kan laves med sædehøjde, der passer dig.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-info">
+                        <p class="card-overskrift">Dansk - hele vejen</p>
+                        <p>Alle vores møbler er produceret på egen fabrik i Danmark.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-info">
+                        <p class="card-overskrift">Gedigent håndværk</p>
+                        <p>Vi har lavet stole i over 100 år. Derfor har vi en lang tradition for godt håndværk.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-info">
+                        <p class="card-overskrift">Kvalitetsstof og læder</p>
+                        <p>Kvalitetsstof fra danske Gabriel og ægte læder fra Sørensen Leather.</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <footer>
+                <div class="footer-info">
+
+                    <div class="footer-section">
+
+                        <p class="footer-title">Om Farstrup</p>
+
+                        <div class="footer-content">
+
+                            <div class="footer-description">
+                                <p>Farstrup er specialister i at tilpasse stole, der tilgodeser dine individuelle behov.</p>
+                                <p>Bemærk at dette er Farstrups overordnede website, og der tages forbehold for tastefejl.</p>
+                                <p>Ønsker du at købe et produkt, eller har du behov for service, er du velkommen til at kontakte en af <a href="https://farstrup.dk/forhandlere/">vores mange forhandlere, som du finder her.</a></p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="footer-section">
+
+                        <p class="footer-title">Kontakt</p>
+
+                        <div class="footer-content">
+
+                            <p class="afdelingsnavn">Farstrup Furniture A/S</p>
+                            <p class="adresse">Farstrupvej 21,</p>
+                            <p class="postnummer-by">5471 Søndersø</p>
+                            <p>CVR-nr: 37217514</p>
+                            <p>Tel: <a href="tel:+4563838383">+45 63838383</a></p>
+                            <p>Mail: <a href="mailto:info@farstrup.dk">info@farstrup.dk</a></p>
+
+                        </div>
+
+                    </div>
+
+                    <div class="footer-section">
+
+                        <p class="footer-title">Åbningstider</p>
+
+                        <div class="footer-content">
+
+                            <p class="footer-sub-title">Kontorets åbningstider:</p>
+
+                            <p class="aabningstid"><span class="dag">Mandag: </span><span class="tid">8:00 – 16:00</span></p>
+                            <p class="aabningstid"><span class="dag">Tirsdag: </span><span class="tid">8:00 – 16:00</span></p>
+                            <p class="aabningstid"><span class="dag">Onsdag: </span><span class="tid">8:00 – 16:00</span></p>
+                            <p class="aabningstid"><span class="dag">Torsdag: </span><span class="tid">8:00 – 16:00</span></p>
+                            <p class="aabningstid idag"><span class="dag">Fredag: </span><span class="tid">8:00 – 15:30</span></p>
+                            <p class="aabningstid"><span class="dag">Lørdag: </span><span class="tid">Lukket</span></p>
+                            <p class="aabningstid"><span class="dag">Søndag: </span><span class="tid">Lukket</span></p>
+                        </div>
+
+                    </div>
+
+                    <div class="footer-section">
+
+                        <p class="footer-title">Sociale medier</p>
+
+                        <div class="footer-content">
+
+
+                            <a class="footer-social-medie" href="https://www.facebook.com/FarstrupFurniture/" target="_blank">
+                                <amp-img src="https://farstrup.dk/wp-content/uploads/facebook-icon.svg" width="30" height="30" alt="" layout="fixed" class="i-amphtml-layout-fixed i-amphtml-layout-size-defined" style="width:30px;height:30px" i-amphtml-layout="fixed">
+                                </amp-img>
+                                <span>Facebook</span>
+                            </a>
+
+
+                            <a class="footer-social-medie" href="https://www.instagram.com/farstrupfurniture/" target="_blank">
+                                <amp-img src="https://farstrup.dk/wp-content/uploads/instagram-icon.svg" width="30" height="30" alt="" layout="fixed" class="i-amphtml-layout-fixed i-amphtml-layout-size-defined" style="width:30px;height:30px" i-amphtml-layout="fixed">
+                                </amp-img>
+                                <span>Instagram</span>
+                            </a>
+
+
+                            <a class="footer-social-medie" href="https://www.linkedin.com/company/farstrup-furniture/" target="_blank">
+                                <amp-img src="https://farstrup.dk/wp-content/uploads/linkedin-icon.svg" width="30" height="30" alt="" layout="fixed" class="i-amphtml-layout-fixed i-amphtml-layout-size-defined" style="width:30px;height:30px" i-amphtml-layout="fixed">
+                                </amp-img>
+                                <span>LinkedIn</span>
+                            </a>
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </footer>
+        </div>
